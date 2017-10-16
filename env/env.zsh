e2210720fa100888ce67a3d6d717f08b3499f135
Johnson Zhang <Johnson9009@163.com>
2017-10-16 21:07:47 +0800

    1. Change spacemacs git repository to dotfiles repository and just link ~/.spacemacs.d to ~/.dotfiles/spacemacs.d 2. Add tmux support, maintian our tmux configuration file "~/.tmux.conf" in git repository. 3. vim 7.4 and later version can read the "vimrc" file in ".vim" so we can put all the configuration files of vim in just one directory like    emacs does. And We source "~/.local.vim" to separate local vim initial file from this general initial file. 4. gdbinit source "~/.local.gdb" to separate local gdb initial file from this general initial file. 5. All the shared aliases are changed to little shell script, because the alias syntax is different between various shells, and the little    shell scripts should specify "/bin/sh" as its execute shell. On Debian and Ubuntu the "/bin/sh" is soft symbolic link to dash. Dash is    faster then bash, besides that dash comply the POSIX standard strictly.    Any symbolic link are not included in git repository. We will add a setup script which can create these symbolic links automatically later. 6. We minimize dependence of a spicific shell like zsh, because maybe in some circumstances we can't use it. Every shell initial file should    source a local initial file such as "~/.local.zsh", so we can put local configrations in this file. And then we export an environment    virable called "DOTFILES" to record the full path of our dotfiles directory. We can get full path of our dotfiles directory automatically.    So we can support clone the dotfiles to any directory. Finally we sourced environment Variables shell initial file such as    "${DOTFILES}/env/env.zsh". This can't be replaced by other language script like python, because child process can't change the environment    of parent process, so we only can use "source" command to load them to the current shell. In addition, each shell need one copy, the file    name should end up with the corresponding shell name like "env.zsh".

---
+++ env/env.zsh
@@ -0,0 +1,3 @@
+export EDITOR=em
+export TERM=xterm-256color
+export PATH=${DOTFILES}/bin:${PATH}
---
+++ gdb/loadLocalInit.py
@@ -0,0 +1,9 @@
+from os import path
+
+localInitFile = path.expanduser('~/.local.gdb')
+if (path.isfile(localInitFile) is True):
+    gdb.execute('source %s' % localInitFile)
+    print('The local initial file %s is loaded.' % localInitFile)
+else:
+    print('If the local initial file %s is exist, it will be loaded automatically.' % localInitFile)
+
