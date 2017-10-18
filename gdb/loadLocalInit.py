170c5a76c994941e909c1610a337c9baf77ef0fb
Johnson Zhang <Johnson9009@163.com>
2017-10-18 12:15:15 +0800

    Chang naming convention of local dot files to achieve consistent name with original dot files. For example, our zshrc will load "~/.local.zshrc" instead of "~/.local.zsh" automatically.

--- gdb/loadLocalInit.py
+++ gdb/loadLocalInit.py
@@ -1,6 +1,6 @@
 from os import path
 
-localInitFile = path.expanduser('~/.local.gdb')
+localInitFile = path.expanduser('~/.local.gdbinit')
 if (path.isfile(localInitFile) is True):
     gdb.execute('source %s' % localInitFile)
     print('The local initial file %s is loaded.' % localInitFile)
