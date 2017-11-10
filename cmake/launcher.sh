2d9379229e7c3c575186c94b5ad5418d58805270
Johnson Zhang <Johnson9009@163.com>
2017-11-11 01:04:38 +0800

    Fix a bug which cause "dash" can't execute "${DOTFILES}/cmake/launcher.sh", "/bin/sh" is symbolic link to "/bin/dash" on Ubuntu, "dash" is faster than "bash", it intends to execute shell script instead of shell interactive and it is very restrict on POSIX standard, so writing "dash" script is harder then writing "bash" script. The method used to get all parameters except the last one can't work in dash, so we use "shift" to eliminate the last parameter when we have at least one parameter.

--- cmake/launcher.sh
+++ cmake/launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-case $(basename $0) in
+case $(basename ${0}) in
     cmkd)
         build_type="Debug"
         ;;
@@ -13,7 +13,7 @@ case $(basename $0) in
         build_type="MinSizeRel"
         ;;
     cmkb)
-        exec cmake --build $@
+        exec cmake --build ${@}
         ;;
     *)
         echo "Error: Running from unknow symbolic link \"$0\", this shell script only can be"
@@ -22,10 +22,10 @@ case $(basename $0) in
         ;;
 esac
 
-if [ $# -eq 0 ]; then
+if [ ${#} -eq 0 ]; then
     target_dirname=$(pwd)
 else
-    eval target_dirname=\${$#}
+    eval target_dirname=\${${#}}
     case ${target_dirname} in
         /*)
             ;;
@@ -33,6 +33,7 @@ else
             target_dirname=$(pwd)/${target_dirname}
             ;;
     esac
+    shift
 fi
 
 if [ -f "${target_dirname}/CMakeLists.txt" ]; then
@@ -40,4 +41,4 @@ if [ -f "${target_dirname}/CMakeLists.txt" ]; then
     cd ${target_dirname}/build/${build_type}
 fi
 
-cmake -DCMAKE_BUILD_TYPE=${build_type} ${@:0:$#} ${target_dirname}
+exec cmake -DCMAKE_BUILD_TYPE=${build_type} ${@} ${target_dirname}
