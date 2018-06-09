9667d0b9f091a7ab473423839a8320da0a82db41
Johnson Zhang <Johnson9009@163.com>
2018-06-09 15:52:14 +0800

    Modify Key Bindings of "auto-completion" & Add "yasnippet-snippets"
    1. "TAB" key is used to skip to next parameter of function in some program mode,
       such as c++-mode, so an alternate is needed. In emacs style key bindings of
       company, "C-f" is used for that, "zsh-autosuggestions" plugin of Zsh uses it
       too, so it is the perfect candidate.
    2. By setting "auto-completion-tab-key-behavior" to "nil" can force users to use
       "C-f", finally users will be used to use "C-f" without thinking and avoid
       skipping to next parameter when they just want to  select the current
       completion.
    3. Now both "C-j", "C-k", "C-n", "C-p" can be used to select the correct one in
       all the complete candidates, "C-f", "C-l", "RET" can be used to accept the
       selected completion. "C-b", "C-g" can be used to give up auto completion and
       escape from the pop-up window.
    4. Even though "auto-completion" layer include the "yasnippet" package, but
       still no any snippets can be used, because Spacemacs don't ship any snippets,
       so package "yasnippet-snippets" is needed, it contains the official
       collection of snippets for yasnippet.

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -41,7 +41,8 @@ values."
           ivy-ignore-buffers '("\\` " "\\`\\*")
           ;; Ignore current dir(.) and father dir(..) in "counsel find file" default.
           ivy-extra-directories nil)
-     auto-completion
+     (auto-completion :variables
+                      auto-completion-tab-key-behavior 'nil)
      better-defaults
      emacs-lisp
      (git :variables
@@ -65,7 +66,8 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(xclip)
+   dotspacemacs-additional-packages '(xclip
+                                      yasnippet-snippets)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
@@ -353,6 +355,18 @@ you should place your code here."
     "bk"  'kill-buffer-and-window
     "w-"  'split-below-and-focus-scratch
     "w/"  'split-right-and-focus-scratch)
+  ;; Set right margin to 100, and turn it on for all text and programming modes.
+  (setq-default fill-column 100)
+  (spacemacs/add-to-hooks 'turn-on-fci-mode '(text-mode-hook prog-mode-hook))
+  ;; Enable company mode globally and enable both vim and emacs style key bindings of company mode.
+  ;; Only key bindings of the style which represented by dotspacemacs-editing-style will be set by
+  ;; default, even though auto-completion layer add "spacemacs//company-active-navigation" to the
+  ;; the hook "spacemacs-editing-style-hook", because this hook will not be executed when evil
+  ;; switch state between normal and emacs. The company mode must be enabled globally, or function
+  ;; "spacemacs//company-active-navigation" can't be called at here.
+  (global-company-mode)
+  (spacemacs//company-active-navigation 'vim)
+  (spacemacs//company-active-navigation 'emacs)
 
   ;; Below settings only have effects for GUI emacs.
 
@@ -368,9 +382,6 @@ you should place your code here."
   ;; Set the highlight background of current line to black, this make selected region become more
   ;; distinct.
   (set-face-background 'hl-line "#000000")
-  ;; Set right margin to 100, and turn it on for all text and programming modes.
-  (setq-default fill-column 100)
-  (spacemacs/add-to-hooks 'turn-on-fci-mode '(text-mode-hook prog-mode-hook))
   )
 
 
@@ -404,7 +415,7 @@ you should place your code here."
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (disaster company-c-headers cmake-mode clang-format org-mime smeargle orgit magit-gitflow gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link evil-magit magit magit-popup git-commit with-editor flycheck-pos-tip pos-tip flycheck yapfify pyvenv pytest pyenv-mode py-isort pip-requirements live-py-mode hy-mode dash-functional cython-mode company-anaconda anaconda-mode pythonic vmd-mode mmm-mode markdown-toc markdown-mode gh-md org-projectile org-category-capture org-present org-pomodoro alert log4e gntp org-download htmlize gnuplot xclip xterm-color unfill shell-pop mwim multi-term fuzzy flyspell-correct-ivy flyspell-correct eshell-z eshell-prompt-extras esh-help company-statistics company auto-yasnippet yasnippet auto-dictionary ac-ispell auto-complete monokai-theme ws-butler winum which-key wgrep volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline smex restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint ivy-hydra info+ indent-guide hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-make google-translate golden-ratio flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu elisp-slime-nav dumb-jump define-word counsel-projectile column-enforce-mode clean-aindent-mode auto-highlight-symbol auto-compile aggressive-indent adaptive-wrap ace-window ace-link))))
+    (yasnippet-snippets disaster company-c-headers cmake-mode clang-format org-mime smeargle orgit magit-gitflow gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link evil-magit magit magit-popup git-commit with-editor flycheck-pos-tip pos-tip flycheck yapfify pyvenv pytest pyenv-mode py-isort pip-requirements live-py-mode hy-mode dash-functional cython-mode company-anaconda anaconda-mode pythonic vmd-mode mmm-mode markdown-toc markdown-mode gh-md org-projectile org-category-capture org-present org-pomodoro alert log4e gntp org-download htmlize gnuplot xclip xterm-color unfill shell-pop mwim multi-term fuzzy flyspell-correct-ivy flyspell-correct eshell-z eshell-prompt-extras esh-help company-statistics company auto-yasnippet yasnippet auto-dictionary ac-ispell auto-complete monokai-theme ws-butler winum which-key wgrep volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline smex restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint ivy-hydra info+ indent-guide hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-make google-translate golden-ratio flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu elisp-slime-nav dumb-jump define-word counsel-projectile column-enforce-mode clean-aindent-mode auto-highlight-symbol auto-compile aggressive-indent adaptive-wrap ace-window ace-link))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
