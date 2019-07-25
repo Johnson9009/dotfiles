796a7853773ee241826a5b590767b74414987734
Johnson9009 <Johnson9009@163.com>
2019-07-25 23:58:08 +0800

    Fix Bug of Setting Environment Variable `SHELL` on Mac OS
    1. Mac OS doesn't support the proc file system natively, so the environment
       variable `SHELL` will not be set, this will cause lots of terminal
       application can't work, e.g. `Emacs`.
    
    2. The proc file system is the most reliable way to get the real full path of
       the current shell, `which xsh` can't ensure to give out the path of the
       current running xsh shell, because the current running xsh shell maybe isn't
       the default xsh shell.
       For example, on company's server the default shell is not xsh, e.g. ysh, and
       we haven't the root or sudo privilege, so we can only change the login shell
       through executing `exec /xxx/xsh -l` in ysh's runtime configuration file,
       e.g. `~/.yshrc`. Notice the path of xsh in command `exec /xxx/xsh -l`,
       thinking this circumstance there are several version of xsh installed on the
       server, but the xsh meet your needs is `/xxx/xsh` not the default xsh, so in
       this circumstance, the shell executing the `env.xsh` is not the default xsh.

--- csh/env.csh
+++ csh/env.csh
@@ -1,3 +1,10 @@
-setenv SHELL `readlink -n /proc/$$/exe`
+# The most reliable way to get the real full path of the current shell is
+# consulting the proc file system, `$$` is the `PID` of the current shell, so
+# `/proc/$$/exe` is linked to the real full path of the current shell. The proc
+# file system way works well on almost every platform, e.g. linux, MSYS2, WSL,
+# and even Android, except Mac OS, because Mac OS doesn't support the proc file
+# system natively, so `which csh` is adopted when the proc file system way
+# failed.
+setenv SHELL `readlink -n /proc/$$/exe || which csh`
 setenv EDITOR em
 setenv TERM xterm-256color
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,3 +1,10 @@
-export SHELL=`readlink -n /proc/$$/exe`
+# The most reliable way to get the real full path of the current shell is
+# consulting the proc file system, `$$` is the `PID` of the current shell, so
+# `/proc/$$/exe` is linked to the real full path of the current shell. The proc
+# file system way works well on almost every platform, e.g. linux, MSYS2, WSL,
+# and even Android, except Mac OS, because Mac OS doesn't support the proc file
+# system natively, so `which zsh` is adopted when the proc file system way
+# failed.
+export SHELL=`readlink -n /proc/$$/exe || which zsh`
 export EDITOR=em
 export TERM=xterm-256color
