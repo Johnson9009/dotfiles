201ec319cb58cc0107390fb39316d140e7977967
Johnson9009 <johnson9009@163.com>
2020-06-16 23:44:32 +0800

    [GDB] Refine Settings of Init File and Add Local Init File Template

--- gdb/loadLocalInit.py
+++
@@ -1,8 +0,0 @@
-from os import path
-
-localInitFile = path.expanduser('~/.local.gdbinit')
-if (path.isfile(localInitFile) is True):
-    gdb.execute('source %s' % localInitFile)
-    print('The local initial file %s is loaded.' % localInitFile)
-else:
-    print('If the local initial file %s is exist, it will be loaded automatically.' % localInitFile)
